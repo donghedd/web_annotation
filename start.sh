b0232b3540ae9583c4876f9d75c840bb3f82463e
donghedd <2069083570@qq.com>
2025-11-04 21:34:01 +0800

    modified:   start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,11 @@
-<<<<<<< HEAD
 #!/bin/bash
-#! bin/bash
-# >>>>>>> 1d8232282a848f759e330463c9665f401ad5861e
 ## author:lee
 ## This shell script is used to automatically start related services for web_annotation system!
 
 # 启动nginx服务
-# <<<<<<< HEAD
 exec nohup nginx -c /root/miniconda3/etc/nginx/nginx.conf > /root/web_annotation/process_log/nginx_process.log &
 echo "nginx start successfully, and then hidden!"
-# 启动gunicorn服务
+
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 __conda_setup="$('/root/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
@@ -28,9 +24,3 @@ unset __conda_setup
 conda activate web_annotation
 exec gunicorn -c '/root/web_annotation/gunicorn.py' first_test:app > /root/web_annotation/process_log/gunicorn_process.log &
 echo "gunicorn start successfully!"
-nginx -c /root/miniconda3/etc/nginx/nginx.conf
-
-# 启动gunicorn服务
-gunicorn -c '/root/web_annotation/gunicorn.py' first_test:app
-# >>>>>>> 1d8232282a848f759e330463c9665f401ad5861e
-
